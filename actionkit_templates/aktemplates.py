4260fb07f6b02b3aee3abc1244a5501a0409443d
Sandra Chung <sandra@moveon.org>
2019-03-20 12:00:00 -0600

    now you can discover tests in the same dir where you're running aktemplates test

--- actionkit_templates/aktemplates.py
+++ actionkit_templates/aktemplates.py
@@ -3,9 +3,10 @@ import os
 import sys
 #STATIC_FALLBACK="/static/js/fallback_local" PYTHONPATH=./djanger django-admin runserver --settings=settings
 
+
 def serve_templates():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "actionkit_templates.settings")
-
+    sys.path.append(os.getcwd()) # run `aktemplate test` from the same dir where tests live
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
