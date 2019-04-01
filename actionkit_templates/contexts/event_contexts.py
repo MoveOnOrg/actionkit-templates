20fb37b9c78cc3f386c2050b440c7213a26203db
Sandra Chung <sandra@moveon.org>
2019-04-01 16:09:18 -0600

    mueller load context

--- actionkit_templates/contexts/event_contexts.py
+++ actionkit_templates/contexts/event_contexts.py
@@ -457,6 +457,51 @@ contexts = {
             "name": "fakecampaign_attend"
         },
     },
+    'event_search_with_mueller_load.html': {
+        "filename": "event_search.html",
+        "form": {
+            "search_page_text": "<p>Search page text for campaign with 1000 events.</p>",
+        },
+        "page": {
+            "title": "Page Title - A Mueller Load of Events",
+            "name": "fakecampaign-mueller_load_events_attend"
+        },
+        "events": [event_create(1, 17, 10000+place, place_index=place) for place in range(1000)],
+        "campaign": {
+            "local_title": "Campaign Title - Campaign with a Mueller Load of events",
+            "local_name": "fakecampaign-mueller_load_events_attend",
+            "use_title": True,
+            "show_venue": True,
+            "show_title": True,
+            "show_zip": True,
+            "show_public_description": True,
+            "name": "fakecampaign-mueller_load_events"
+        },
+    },
+    'event_search_with_mueller_load': {
+        "filename": "event_search.html",
+        "args": {
+            "page": "event_search"
+        },
+        "campaign": {
+            "local_title": "Campaign Title",
+            "local_name": "fakecampaign-mueller_load_events_attend",
+            "use_title": True,
+            "show_venue": True,
+            "show_title": True,
+            "show_zip": True,
+            "show_public_description": True,
+            "name": "fakecampaign-mueller_load_events"
+        },
+        "events": [event_create(1, 17, 10000+place, place_index=place) for place in range(1000)],
+        "form": {
+            "search_page_text": "<p>Search page text for campaign with only past events.</p>",
+        },
+        "page": {
+            "title": "Page Title",
+            "name": "fakecampaign_attend"
+        },
+    },
     'event_create.html': {
         "filename": "event_create.html",
         'page': {
