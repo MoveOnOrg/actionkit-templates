78a8d0575f1b69fd6d8f486c246645a8a48a5c03
Kathy Nguyen <kathymn@gwu.edu>
2020-10-28 12:49:13 -0400

    Change do-not-mail to lost-pages-redesign

--- actionkit_templates/contexts/survey_contexts.py
+++ actionkit_templates/contexts/survey_contexts.py
@@ -1,46 +1,5 @@
 from django.utils.html import format_html
 
-do_not_mail = {
-    'filename': 'survey.html',
-    'page': {
-        "title": "Change Do Not Mail Status",
-        "canonical_url": "http://example.com/survey/foobar",
-        "custom_fields": {
-            "layout_options": "lost-pages-redesign"
-        }
-    },
-    'form': {
-        'introduction_text': 'Marking your account as "Do Not Mail" means MoveOn will not send you postal mail. (Your email address is required only to communicate with you if there are issues processing your opt-out—completing this form will not subscribe you to MoveOn\'s email list.)',
-    },
-    'user_fields': [
-        {'field_name': 'email',
-         'label_text': 'Email Address',
-         'input_tag': '<input id="id_email" type="text" class="form-control mo-userfield-input ak-has-overlay"  name="email" />',
-         'input_html': format_html('<input id="id_email" type="text" class="form-control mo-userfield-input ak-has-overlay"  name="email" />'),
-        },
-        {'field_name': 'name',
-         'label_text': 'Name',
-         'input_tag': '<input id="id_name" type="text" class="form-control mo-userfield-input ak-has-overlay" name="name" />',
-         'input_html': format_html('<input id="id_name" type="text" class="form-control mo-userfield-input ak-has-overlay" name="name" />'),
-        },
-        {'field_name': 'address1',
-         'label_text': 'Street Address',
-         'input_tag': '<input id="id_address1" type="text" class="form-control mo-userfield-input ak-has-overlay" />',
-         'input_html': format_html('<input id="id_address1" type="text" class="form-control mo-userfield-input ak-has-overlay" />'),
-        },
-        {'field_name': 'zip',
-         'label_text': 'ZIP Code',
-         'input_tag': '<input id="id_zip" type="text" class="form-control mo-userfield-input ak-has-overlay" name="zip" />',
-         'input_html': format_html('<input id="id_zip" type="text" class="form-control mo-userfield-input ak-has-overlay" name="zip" />'),
-        },
-        {'field_name': 'phone',
-         'label_text': 'Phone',
-         'input_tag': '<input id="id_phone" type="text" class="form-control mo-userfield-input ak-has-overlay" name="phone" />',
-         'input_html': format_html('<input id="id_phone" type="text" class="form-control mo-userfield-input ak-has-overlay" name="phone" />'),
-     },
-    ],
-}
-
 logged_in_data_with_hide_recognized_block = {
     'filename': 'survey.html',
     'page': {
@@ -79,6 +38,47 @@ logged_in_data_with_hide_recognized_block = {
          'label_text': 'Email Address',
          'input_tag': '<input id="id_email" type="text" class="form-control mo-userfield-input ak-has-overlay"  name="email" />',
          'input_html': format_html('<input id="id_email" type="text" class="form-control mo-userfield-input ak-has-overlay"  name="email" />'),
+        },
+        {'field_name': 'address1',
+         'label_text': 'Street Address',
+         'input_tag': '<input id="id_address1" type="text" class="form-control mo-userfield-input ak-has-overlay" />',
+         'input_html': format_html('<input id="id_address1" type="text" class="form-control mo-userfield-input ak-has-overlay" />'),
+        },
+        {'field_name': 'zip',
+         'label_text': 'ZIP Code',
+         'input_tag': '<input id="id_zip" type="text" class="form-control mo-userfield-input ak-has-overlay" name="zip" />',
+         'input_html': format_html('<input id="id_zip" type="text" class="form-control mo-userfield-input ak-has-overlay" name="zip" />'),
+        },
+        {'field_name': 'phone',
+         'label_text': 'Phone',
+         'input_tag': '<input id="id_phone" type="text" class="form-control mo-userfield-input ak-has-overlay" name="phone" />',
+         'input_html': format_html('<input id="id_phone" type="text" class="form-control mo-userfield-input ak-has-overlay" name="phone" />'),
+     },
+    ],
+}
+
+lost_pages_redesign = {
+    'filename': 'survey.html',
+    'page': {
+        "title": "Change Do Not Mail Status",
+        "canonical_url": "http://example.com/survey/foobar",
+        "custom_fields": {
+            "layout_options": ["lost-pages-redesign"]
+        }
+    },
+    'form': {
+        'introduction_text': 'Marking your account as "Do Not Mail" means MoveOn will not send you postal mail. (Your email address is required only to communicate with you if there are issues processing your opt-out—completing this form will not subscribe you to MoveOn\'s email list.)',
+    },
+    'user_fields': [
+        {'field_name': 'email',
+         'label_text': 'Email Address',
+         'input_tag': '<input id="id_email" type="text" class="form-control mo-userfield-input ak-has-overlay"  name="email" />',
+         'input_html': format_html('<input id="id_email" type="text" class="form-control mo-userfield-input ak-has-overlay"  name="email" />'),
+        },
+        {'field_name': 'name',
+         'label_text': 'Name',
+         'input_tag': '<input id="id_name" type="text" class="form-control mo-userfield-input ak-has-overlay" name="name" />',
+         'input_html': format_html('<input id="id_name" type="text" class="form-control mo-userfield-input ak-has-overlay" name="name" />'),
         },
         {'field_name': 'address1',
          'label_text': 'Street Address',
@@ -214,7 +214,7 @@ contexts = {
     'survey_logged_in_hide_recognized': logged_in_data_with_hide_recognized_block,
     'survey_no_questions': no_questions,
     'survey_no_survey': no_survey,
-    'survey_do-not-mail.html': do_not_mail,
+    'survey_lost-pages-redesign.html': lost_pages_redesign,
      #kinda silly, but avoid appending to the bottom because then git merge conflicts arise more often.
     #let's do the context values in alphabetical order.
 }
