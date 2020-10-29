95a788ef383fc7e39f322d052fb4a890f6b3c89a
Kathy Nguyen <kathymn@gwu.edu>
2020-10-29 15:40:55 -0400

    Test Post Mail Status input_tag

--- actionkit_templates/contexts/survey_contexts.py
+++ actionkit_templates/contexts/survey_contexts.py
@@ -78,7 +78,7 @@ lost_pages_redesign = {
         },
         {'field_name': 'user_postal_mail_status',
          'label_text': 'Postal Mail Status',
-         'input_tag': lib.postal_mail_statuses(),
+         'input_tag': lib.states(),
         },
         {'field_name': 'name',
          'label_text': 'Name',
