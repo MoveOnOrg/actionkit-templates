2e761d854856487ab01ad56bfacccde09d412e4d
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-12-19 04:24:25 +0000

    clear old tests

--- tests/tests.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-class TemplateTest:
-    def test_donate(self):
-        self.open("/donations/donate.1?amounts=17-39-51-other")
-        # test whether 'donation_type' text is present likely for choosing/setting the donation_type
-        self.assertTrue(re.search(r"donation_type", self.wd.page_source))
