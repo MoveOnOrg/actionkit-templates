df903eb56ec66a06a76d0b12544ab4d2f2e81bc6
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-12-19 07:20:41 +0000

    extract urls.py

--- dsa_actionkit/settings.py
+++ dsa_actionkit/settings.py
@@ -212,33 +212,7 @@ def proxy_serve(request, path, document_root=None, show_indexes=False):
     raise Http404
 
 
-#############
-# URLS
-#############
 
-ROOT_URLCONF = 'dsa_actionkit.settings'
-
-urlpatterns = [
-    re_path(r'^context', login_context),
-    re_path(r'^progress', login_context, name='progress'),
-    re_path(r'^logout', logout, name="logout"),
-    re_path(r'^(?P<name>[-.\w]+)?(/(?P<page>[-.\w]+))?$', index),
-    re_path(r'^forgot/$', user_password_forgot, name='user_password_forgot'),
-    re_path(r'^cms/event/(?P<page>[-.\w]+)/search_results/', event_search_results, name='event_search_results'),
-    re_path(r'^fake/api/events', event_api_moveon_fake, name="event_api_moveon_fake"),
-    # ActionKit urls or {% url %} template tag:
-    re_path(r'^fake/stub/reverse', event_api_moveon_fake, name="reverse_donation"),
-]
-if STATIC_ROOT:
-    urlpatterns = (urlpatterns
-                   + static(STATIC_URL, document_root=STATIC_ROOT)
-                   + static('/resources/',
-                            view=proxy_serve,
-                            document_root=os.path.join(STATIC_ROOT, './resources'))
-                   + static('/media/',
-                            view=proxy_serve,
-                            document_root=os.path.join(STATIC_ROOT, './media'))
-    )
 
 if os.path.exists(os.path.join(PROJECT_ROOT_PATH, 'local_settings.py')):
     from local_settings import *
@@ -246,6 +220,6 @@ if os.path.exists(os.path.join(PROJECT_ROOT_PATH, 'local_settings.py')):
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+        "NAME": "db.sqlite",
     }
 }
---
+++ dsa_actionkit/urls.py
@@ -0,0 +1,27 @@
+#############
+# URLS
+#############
+
+ROOT_URLCONF = 'dsa_actionkit.urls'
+
+urlpatterns = [
+    re_path(r'^context', login_context),
+    re_path(r'^progress', login_context, name='progress'),
+    re_path(r'^logout', logout, name="logout"),
+    re_path(r'^(?P<name>[-.\w]+)?(/(?P<page>[-.\w]+))?$', index),
+    re_path(r'^forgot/$', user_password_forgot, name='user_password_forgot'),
+    re_path(r'^cms/event/(?P<page>[-.\w]+)/search_results/', event_search_results, name='event_search_results'),
+    re_path(r'^fake/api/events', event_api_moveon_fake, name="event_api_moveon_fake"),
+    # ActionKit urls or {% url %} template tag:
+    re_path(r'^fake/stub/reverse', event_api_moveon_fake, name="reverse_donation"),
+]
+if STATIC_ROOT:
+    urlpatterns = (urlpatterns
+                   + static(STATIC_URL, document_root=STATIC_ROOT)
+                   + static('/resources/',
+                            view=proxy_serve,
+                            document_root=os.path.join(STATIC_ROOT, './resources'))
+                   + static('/media/',
+                            view=proxy_serve,
+                            document_root=os.path.join(STATIC_ROOT, './media'))
+    )
