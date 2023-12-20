ed6d957002fc66f066ebe0828d5ddb7b6973b025
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-12-20 14:52:41 +0000

    use suggested list in pytest parametrize

--- tests/test_user_landing_page.py
+++ tests/test_user_landing_page.py
@@ -1,8 +1,8 @@
 import pytest
-from django.shortcuts import reverse
+from django.urls import reverse
 
 
-@pytest.mark.parametrize("dues_amount", (3, 100))
+@pytest.mark.parametrize("dues_amount", [(3, 100)])
 def test_dues_amount_correct_after_logging_in(django_user_model, client, dues_amount):
     member = django_user_model.object.create(status="M", dues_amount=dues_amount)
     client.force_login(member)
